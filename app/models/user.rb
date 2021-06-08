f8c804e6c66c4208325446c231cffd2c6e4cfc84
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-08 21:20:34 +0900

    ユーザー新規登録バリデーション修正1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,16 @@ class User < ApplicationRecord
 
   validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end

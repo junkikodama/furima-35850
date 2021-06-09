1e637c6930ee3afa3b196eddf9481c8c138d52c1
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-09 16:15:57 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,24 @@
 class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category_id,       null: false
-  belongs_to_active_hash :status_id,         null: false
-  belongs_to_active_hash :charge_id,         null: false
-  belongs_to_active_hash :prefecture_id,     null: false
-  belongs_to_active_hash :duration_id,       null: false
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :duration
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :duration_id
+    validates :price
+  end
 end

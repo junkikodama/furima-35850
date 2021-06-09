5cfdc97da30b35972811501c7c208e805ed5dcfd
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-09 15:48:44 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category_id,       null: false
+  belongs_to_active_hash :status_id,         null: false
+  belongs_to_active_hash :charge_id,         null: false
+  belongs_to_active_hash :prefecture_id,     null: false
+  belongs_to_active_hash :duration_id,       null: false
+
   belongs_to :user
   has_one_attached :image
 end

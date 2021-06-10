633a0e85c695aa2e4cfd10b72cbef0afce7f248b
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-10 10:39:50 +0900

    不要なクオーテーション削除及び価格の半角英数混合、英語のみテスト追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,27 +29,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリー(category_id)が空では出品出来ない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品状態(status_id)が空では出品出来ない' do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it '配送料負担(charge_id)が空では出品出来ない' do
-        @item.charge_id = '1'
+        @item.charge_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Charge must be other than 1')
       end
       it '発送元地域情報(prefecture_id)が空では出品出来ない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '発送必要日数(duration_id)が空では出品出来ない' do
-        @item.duration_id = '1'
+        @item.duration_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Duration must be other than 1')
       end
@@ -59,12 +59,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格が300円未満では出品出来ない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
       it '価格が9,999,999円より高い場合は出品出来ない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
@@ -73,7 +73,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-
+      it '価格が半角英数混合では出品出来ない' do
+        @item.price = 'ab12'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '価格が英語のみでは出品出来ない' do
+        @item.price = 'abcd'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
     end
   end
 end

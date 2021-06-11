6de9072f904a9804247c73d5d4fb6923f321045b
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-11 11:53:32 +0900

    before_actionを用いたコードの省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,21 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!,except: [:index,:show]
+before_action :item_find ,only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
       if @item.update(item_params)
         redirect_to item_path(@item.id)
@@ -40,7 +38,13 @@ before_action :authenticate_user!,except: [:index,:show]
     end
   end
 
+  private
   def item_params
     params.require(:item).permit(:name, :price, :image, :description, :category_id, :status_id, :charge_id, :prefecture_id, :duration_id).merge(user_id: current_user.id)
   end
+
+  def item_find
+    @item = Item.find(params[:id])
+  end
+
 end

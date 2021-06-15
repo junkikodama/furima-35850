84d9e4c1717b4db56ef4e43718f3b9257655e712
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-15 21:40:16 +0900

    購入時エラーメッセージのitem can't be blank削除の為

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -19,7 +19,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block, :build, :phone_number).merge(user_id: current_user.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block, :build, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def item_find

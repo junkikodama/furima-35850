eed900dfe93346170bb836f6c97d898cfc9a0c0a
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-15 21:14:50 +0900

    商品購入時エラーメッセージの表示

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,7 +6,6 @@ class PurchaseAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :city
     validates :block
-    validates :build
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id

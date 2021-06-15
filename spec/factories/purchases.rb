b35cbc1920bd07a880271c8da54e6263f8f65151
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-15 21:58:56 +0900

    購入に必要なファクトリーボットの設定

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :purchase do
+    postal_code {545-0021}
+    city {'大阪市阿倍野区阪南町3丁目'}
+    block {'26-2'}
+    prefecture_id {28}
+    phone_number {0666246800}
+    association :user, :item
     
   end
 end

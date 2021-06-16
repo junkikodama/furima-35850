e383cd909a594d4f5d4b07a0e2dc8e3880d22741
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-16 19:40:57 +0900

    クレカ機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -12,6 +12,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      purchase_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -22,7 +23,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block, :build, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :block, :build, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def item_find
@@ -39,6 +40,15 @@ class PurchasesController < ApplicationController
     if @item.user_id == current_user.id
       redirect_to root_path
     end
+  end
+
+  def purchase_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end

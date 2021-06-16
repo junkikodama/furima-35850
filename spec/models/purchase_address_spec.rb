da7210d7fdfc5d7476f0dfc38d3e4a7172901a05
junkikodama <84299468+junkikodama@users.noreply.github.com>
2021-06-16 20:38:44 +0900

    購入時user及びitemのidに関する単体テストコード追加

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -94,6 +94,18 @@ RSpec.describe PurchaseAddress, type: :model do
           @purchase.valid?
           expect(@purchase.errors.full_messages).to include("Token can't be blank")
         end
+
+        it 'user_idが空では商品購入出来ない' do
+          @purchase.user_id = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("User can't be blank")
+        end
+
+        it 'item_idが空では商品購入出来ない' do
+          @purchase.item_id = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Item can't be blank")
+        end
       end
     end
 end
